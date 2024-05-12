a1ad94b4dbe97a6d8c80f8db04a42cbd5609044c
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-12 12:47:52 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+USE [db_12.05]
+GO
+
+INSERT INTO Stationery (Name, Type, Quantity, Cost_per_Unit) 
+VALUES 
+    ('Pens', 'Writing Instruments', 100, 0.5),
+    ('Notebooks', 'Paper Products', 50, 1.5),
+    ('Staplers', 'Desk Accessories', 20, 3.0);
+
+INSERT INTO Sales (Stationery_ID, Sales_Manager, Buyer_Company, Manager_Selling, Quantity_Sold, Price_Per_Unit, Sale_Date) 
+VALUES 
+    (1, 'John Smith', 'ABC Company', 'Alice Johnson', 50, 1.0, '2024-05-01'),
+    (2, 'Jane Doe', 'XYZ Corporation', 'Bob Williams', 30, 2.0, '2024-05-02'),
+    (3, 'Michael Brown', '123 Inc.', 'Emily Davis', 10, 4.0, '2024-05-03');
